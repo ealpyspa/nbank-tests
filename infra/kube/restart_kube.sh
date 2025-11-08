d31ce75c2ea980232066db88a21c4841730ca3e5
ealpyspa <alpyspaeva.en@gmail.com>
2025-11-08 09:55:18 +0100

    Add prometheus and grafana

--- infra/kube/restart_kube.sh
+++ infra/kube/restart_kube.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# STEP-1: Run application services
 # Started local kubernetes cluster via minikube using Docker as driver
 # cluster is running inside docker container
 minikube start --driver=docker
@@ -28,3 +29,22 @@ kubectl port-forward svc/frontend 3000:80 # > /dev/null 2>&1 & (port-forward in 
 kubectl port-forward svc/backend 4111:4111
 kubectl port-forward svc/selenoid 4444:4444
 kubectl port-forward svc/selenoid-ui 8080:8080
+
+# STEP-2: Run monitoring services
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts || true
+helm repo add elastic https://helm.elastic.co || true
+helm repo update
+
+helm upgrade --install monitoring prometheus-community/kube-prometheus-stack -n monitoring --create-namespace -f monitoring-values.yaml
+
+# Port forward to Prometheus and Grafana
+kubectl port-forward svc/monitoring-kube-prometheus-prometheus -n monitoring 3001:9090
+kubectl port-forward svc/monitoring-grafana -n monitoring 3002:80
+
+# Create secrets for authorization on backend
+kubectl create secret generic backend-basic-auth --from-literal=username=admin --from-literal=password=admin -n monitoring
+
+# Apply yaml with spring-monitoring configuration (for backend)
+kubectl apply -f spring-monitoring.yaml
+

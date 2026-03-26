dcd262dae82d3e0e36761d1307e2812ce0c6c9bd
ealpyspa <alpyspaeva.en@gmail.com>
2026-03-26 11:10:29 +0100

    Improve ui tests - phase3 - iteration2

--- src/test/java/ui/iteration2/TransferMoneyTest.java
+++ src/test/java/ui/iteration2/TransferMoneyTest.java
@@ -4,8 +4,9 @@ import api.generators.RandomModelGenerator;
 import api.models.CreateAccountResponse;
 import api.models.CreateUserRequest;
 import api.models.DepositMoneyRequest;
-import api.requests.steps.AdminSteps;
 import api.requests.steps.UserSteps;
+import common.annotations.UserSession;
+import common.storage.SessionStorage;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import ui.BaseUiTest;
@@ -18,13 +19,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TransferMoneyTest extends BaseUiTest {
     @ParameterizedTest
     @CsvSource("1, 1")
+    @UserSession(value = 2, auth = 1)
     public void userCanMakeTransferToAccountTest(float balance, float amount) {
-        CreateUserRequest user1 = AdminSteps.createUser().getRequest();
+        CreateUserRequest user1 = SessionStorage.getUser(1);
+        CreateUserRequest user2 = SessionStorage.getUser(2);
 
         CreateAccountResponse createAccountResponse1 = UserSteps.userCreatesAccount(user1);
-
         long createdAccountId = createAccountResponse1.getId();
-
         String createdAccountNumber = createAccountResponse1.getAccountNumber();
 
         DepositMoneyRequest depositMoneyRequest = DepositMoneyRequest.builder()
@@ -33,42 +34,32 @@ public class TransferMoneyTest extends BaseUiTest {
                 .build();
 
         UserSteps.depositMoneyResponse(user1, depositMoneyRequest);
-
-        float afterDepositBalance = new UserSteps(user1.getUsername(), user1.getPassword()).getAllAccounts().getFirst().getBalance();
-
-        CreateUserRequest user2 = AdminSteps.createUser().getRequest();
+        float afterDepositBalance = SessionStorage.getSteps(1).getAllAccounts().getFirst().getBalance();
 
         CreateAccountResponse createAccountResponse2 = UserSteps.userCreatesAccount(user2);
-
         String createdAccount2Number = createAccountResponse2.getAccountNumber();
-
         float beforeTransferBalance = createAccountResponse2.getBalance();
-
-        //authAsUser(user1);
 
         // Alert checks by pattern (without amount and account2Number checks)
         new UserDashboard().open().makeTransferClick().getPage(MakeTransferPage.class)
                 .makeTransfer(createdAccountNumber, user2.getUsername(), createdAccount2Number, String.valueOf(amount))
                 .checkAlertAndAcceptMatches(BankAlert.MONEY_TRANSFERRED.getMessage());
 
-        float afterTransferBalance1 = new UserSteps(user1.getUsername(), user1.getPassword()).getAllAccounts().getFirst().getBalance();
-
+        float afterTransferBalance1 = SessionStorage.getSteps(1).getAllAccounts().getFirst().getBalance();
         assertThat(afterTransferBalance1).isEqualTo(afterDepositBalance - amount);
 
-        float afterTransferBalance2 = new UserSteps(user2.getUsername(), user2.getPassword()).getAllAccounts().getFirst().getBalance();
-
+        float afterTransferBalance2 = SessionStorage.getSteps(2).getAllAccounts().getFirst().getBalance();
         assertThat(afterTransferBalance2).isEqualTo(beforeTransferBalance + amount);
     }
 
     @ParameterizedTest
     @CsvSource("1, 1")
+    @UserSession
     public void userCannotMakeTransferToNotExistingAccount(float amount, float balance) {
-        CreateUserRequest user1 = AdminSteps.createUser().getRequest();
+        CreateUserRequest user1 = SessionStorage.getUser();
 
         CreateAccountResponse createAccountResponse1 = UserSteps.userCreatesAccount(user1);
-
         long createdAccountId = createAccountResponse1.getId();
-
         String createdAccountNumber = createAccountResponse1.getAccountNumber();
 
         DepositMoneyRequest depositMoneyRequest = DepositMoneyRequest.builder()
@@ -77,19 +68,15 @@ public class TransferMoneyTest extends BaseUiTest {
                 .build();
 
         UserSteps.depositMoneyResponse(user1, depositMoneyRequest);
-
-        float afterDepositBalance = new UserSteps(user1.getUsername(), user1.getPassword()).getAllAccounts().getFirst().getBalance();
+        float afterDepositBalance = SessionStorage.getSteps().getAllAccounts().getFirst().getBalance();
 
         String randomUsernameAndAccountNumber = RandomModelGenerator.generate(CreateUserRequest.class).getUsername(); // same value for randomUserAccount
-
-        //authAsUser(user1);
 
         new UserDashboard().open().makeTransferClick().getPage(MakeTransferPage.class)
                 .makeTransfer(createdAccountNumber, randomUsernameAndAccountNumber, randomUsernameAndAccountNumber, String.valueOf(amount))
                 .checkAlertAndAccept(BankAlert.NO_USER_FOUND_WITH_ACCOUNT_NUMBER.getMessage());
 
-        float afterTransferBalance1 = new UserSteps(user1.getUsername(), user1.getPassword()).getAllAccounts().getFirst().getBalance();
-
+        float afterTransferBalance1 = SessionStorage.getSteps().getAllAccounts().getFirst().getBalance();
         assertThat(afterTransferBalance1).isEqualTo(afterDepositBalance);
     }
 }
